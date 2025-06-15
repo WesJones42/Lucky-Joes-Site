bc3bfd8fe6aae85f13ecaa60b0e74ffa4879cce7
Wes <97007205+WesJones42@users.noreply.github.com>
2025-06-15 14:45:29 -0600

    Add Google calendar page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { Home } from './home';
 import { Calenders } from './calenders';
 import { MenuComponent } from './pages/menu/menu';
 import { DrinksComponent } from './pages/drinks/drinks';
+import { CalendarComponent } from './pages/calendar/calendar';
 
 export const routes: Routes = [
   { path: '', component: Home },
   { path: 'calenders', component: Calenders },
+  { path: 'calendar', component: CalendarComponent },
   { path: 'menu', component: MenuComponent },
   { path: 'drinks', component: DrinksComponent },
 
