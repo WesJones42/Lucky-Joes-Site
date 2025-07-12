7f5edb68b815933f1aefe4bb668fdef7d1fb436f
WesJones42 <wesjones11@comcast.net>
2025-07-12 16:35:14 -0600

    Redo homepage for mobile devices

--- src/app/home.ts
+++ src/app/home.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { SlideShow } from './slideshow';
 
 @Component({
@@ -8,25 +8,22 @@ import { SlideShow } from './slideshow';
   styleUrls: ['./home.css'],
   imports: [SlideShow]
 })
-export class Home {
-  slide1 = [
-    'B-Roll1.jpg',
-    'B-Roll2.jpg',
-    'B-Roll3.jpg'
-  ];
+export class Home implements AfterViewInit {
+  slide1 = ['B-Roll1.jpg', 'B-Roll2.jpg', 'B-Roll3.jpg'];
+  slide2 = ['B-Roll4.jpg', 'B-Roll5.jpg', 'B-Roll6.jpg'];
+  slide3 = ['B-Roll7.jpg', 'B-Roll8.jpg', 'B-Roll9.jpg'];
 
-  slide2 = [
-    'B-Roll4.jpg',
-    'B-Roll5.jpg',
-    'B-Roll6.jpg'
-  ];
-  slide3 = [
-    'B-Roll7.jpg',
-    'B-Roll8.jpg',
-    'B-Roll9.jpg'
-  ];
+  ngAfterViewInit(): void {
+    const today = new Date().getDay();
+    const specials = document.querySelectorAll("#daily-special .sub-text");
+
+    console.log("Today's day is", today);
+    console.log("Found", specials.length, "specials");
+
+    specials.forEach((el) => {
+      if (Number(el.getAttribute("data-day")) === today) {
+        (el as HTMLElement).classList.add('special-today');
+      }
+    });
+  }
 }
-
-
-
-
