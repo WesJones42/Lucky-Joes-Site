377fea14f64181fd03854e62b9790edf3082c019
WesJones42 <wesjones11@comcast.net>
2025-10-11 12:41:06 -0600

    set up temp merch page

---
+++ src/app/pages/merch1/merch1.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-merch1',
+  imports: [],
+  templateUrl: './merch1.html',
+  styleUrl: './merch1.css',
+})
+export class Merch1Component {
+
+}
