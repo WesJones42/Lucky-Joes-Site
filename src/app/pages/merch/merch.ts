f8bde99abd70a6b0b84ed6146f0143fd188441db
Wes <97007205+WesJones42@users.noreply.github.com>
2025-11-23 10:55:15 -0700

    Revert "Dev2.0"

--- src/app/pages/merch/merch.ts
+++ src/app/pages/merch/merch.ts
@@ -147,7 +147,6 @@ export class MerchComponent implements AfterViewInit, OnDestroy {
             'padding-top' : '15px',
             'padding-right': '5px',
             'padding-left': '5px',
-            'min-height': '710px',
           },
             optionLabel: {
               'color': '#C5B358',      // your gold tone
@@ -231,8 +230,6 @@ export class MerchComponent implements AfterViewInit, OnDestroy {
             paddingLeft: '54px',
             paddingRight: '54px',
             background: '#C5B358',
-            'align-items': 'stretch',
-
           },
         },
         googleFonts: ['Lato'],
